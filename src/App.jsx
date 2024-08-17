52bc2ffabf2b70c2ac14af713545e3361722f8b0
Iryna <irenpd45@gmail.com>
2024-08-17 22:55:33 +0300

    Initial project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Header, Body, Footer } from './components';
+
+
+export const App = () => {
+    return (
+        <>
+            <Header />
+            <Body />
+            <Footer />
+            {/* <Footer /> Alt+Shift+вниз/вверх клонировать */}
+        </>
+    );
+};
---
+++ src/components/Body/Body.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react';
+import './Body.css';
+
+export const Body = () => {
+    const [toggle, setToggle] = useState(false);
+    const title = `title${toggle ? ' title-white' : ''}`;
+
+    const handleToggle = () => setToggle((prev) => !prev);
+
+    useEffect(() => {
+        console.log(toggle);
+    }, [toggle]);
+
+    return (
+        <main className="main">
+            <div className="container">
+                <h2 className={title}>Title</h2>
+                <p className={`text${toggle ? '' : ' hide'}`}>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Doloremque amet voluptate, ut architecto modi consequatur
+                    quidem assumenda quos nesciunt veritatis. Animi labore
+                    voluptatem dolor odit unde ducimus distinctio aspernatur
+                    inventore.
+                </p>
+
+                <button onClick={handleToggle} className="btn-submit">
+                    Submit
+                </button>
+            </div>
+        </main>
+    );
+};
