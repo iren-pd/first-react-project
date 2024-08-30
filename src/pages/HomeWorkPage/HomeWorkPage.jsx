28a2a3a5caa7378e98a01366c0e1631524e29752
iren-pd <irenpd45@gmail.com>
2024-08-30 20:18:47 +0300

    Add some code

--- src/pages/HomeWorkPage/HomeWorkPage.jsx
+++ src/pages/HomeWorkPage/HomeWorkPage.jsx
@@ -1,16 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button } from '../../ui';
 
 export const HomeWorkPage = () => {
     const [num, setNum] = useState(0);
+    const [text, setText] = useState(false);
     const handler = (operation) => {
         if (operation === 'minus') setNum((prev) => prev - 1);
         if (operation === 'plus') setNum((prev) => prev + 1);
     };
 
+    useEffect(() => {
+        num !== 0 ? setText(true) : setText(false);
+    }, [num]);
+
     return (
         <div>
             <p>Твое число: {num}</p>
+            {text && <p>Число равно {num}</p>}
             <div className="container flex">
                 <Button
                     onClick={() => handler('minus')}
